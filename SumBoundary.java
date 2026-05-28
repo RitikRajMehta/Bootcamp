c606c700db2467c9c3556b5ae939f079b95f60ba
RitikRajMehta <ritikrajmehta111@gmail.com>
2026-05-28 19:38:31 +0530

    add

--- SumBoundary.java
+++ SumBoundary.java
@@ -1,35 +1,37 @@
 import java.util.Scanner;
-public class SumBoundary{
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter Size of the matrix: ");
-        int m = scanner.nextInt();
-        int n = scanner.nextInt();
+
+class SumBoundary {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
         int[][] matrix = new int[m][n];
-        System.out.println("Enter the elements of the matrix:");
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                matrix[i][j]=scanner.nextInt();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
             }
         }
-        System.out.println("Matrix: ");
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                System.out.print(matrix[i][j]);
+        System.out.println("Matrix:");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
-        System.out.println("Sum of boundary elements:");
-        for(int i=0;i<m;i++){
-            for(int j=0;i<n;j++){
-                if(i==0 || i==m-1 || j==0 || j==n-1){
-                    System.out.print(matrix[i][j]);
-                }else{
-                    System.out.print(" ");
+        int sum = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (i == 0 || j == 0 || i == m - 1 || j == n - 1) {
+                    sum += matrix[i][j];
                 }
-                System.out.println();
             }
         }
-        scanner.close();
+        System.out.println("Sum = " + sum);
+        sc.close();
     }
 }
