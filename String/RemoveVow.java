c4d9d95906e6f1c0bbcb1e468399dce4c5f91634
RitikRajMehta <ritikrajmehta111@gmail.com>
2026-05-31 01:27:35 +0530

    add

--- String/RemoveVow.java
+++ String/RemoveVow.java
@@ -3,14 +3,14 @@ public class RemoveVow{
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
 
-        String str = new String();
-        str = sc.nextLine();
+        String str = sc.nextLine();
+        String res= "";
         for(int i=0;i<str.length();i++){
-            char ch= Character.toLowerCase(str.charAt(i));
-            if(ch!='a' && ch!='e' && ch!='i' && ch!='o' && ch!='u'){
-                 System.out.print(ch);
+            char ch= str.charAt(i);
+            if("aeiouAEIOU".indexOf(ch) == -1){
+                 res = res + ch;
             }
         }
-        sc.close();
+        System.out.println(res);
     }
 }
