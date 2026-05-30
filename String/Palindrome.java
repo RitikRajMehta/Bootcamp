a34781f79e07a7d84f2a8c56add1eedcf076f24e
RitikRajMehta <ritikrajmehta111@gmail.com>
2026-05-31 01:22:15 +0530

    add

---
+++ String/Palindrome.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Palindrome{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String str = new String();
+        str = sc.nextLine();
+        String rev = new String();
+
+        for(int i=str.length()-1;i>=0;i--){
+            rev = rev + str.charAt(i);
+        }
+
+        if(str.equals(rev)){
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println("Not a Palindrome");
+        }
+    }
+}
---
+++ String/Replace.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Replace{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String str = new String();
+        String str1 = new String();
+        str = sc.nextLine();
+        str1 = sc.nextLine();
+        
+        for(int i=0;i)
+
+       
+    }
+}
