4ab85dfa09473987bd54b86539d4c66a6c115749
RitikRajMehta <ritikrajmehta111@gmail.com>
2026-06-25 23:27:49 +0530

    add

---
+++ Array/Missing.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Missing {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 5};
+
+        int n = 5;
+        int total = n * (n + 1) / 2;
+
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+
+        System.out.println(total - sum);
+    }
+}
